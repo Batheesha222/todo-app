b9272097c6df0495fcb655b1fa8338a74771909d
Batheesha222 <batheesha.dilmeen@gmail.com>
2024-06-09 22:08:00 +0530

    connect with database

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 
 const PORT  =  8000;
 //init app
 const app = express();
 
+const connectionUrl = "mongodb://0.0.0.0:27017/todoDb";
+
+mongoose
+  .connect(connectionUrl)
+  .then(() => console.log("database connection successful"))
+  .catch((error) => console.log(error.message));
+
+  //view engin
 app.set("view engine", "ejs");
 
+// listen sever
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
 })
