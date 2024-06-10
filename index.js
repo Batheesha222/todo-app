6536a0bf1a278cf3d37a04f85e2dfb6c96a503c3
Batheesha222 <batheesha.dilmeen@gmail.com>
2024-06-10 23:06:21 +0530

    todo model schema

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const { timeStamp } = require("console");
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path")
+const path = require("path");
+const { title } = require("process");
 
-const PORT  =  8000;
+const PORT = 8000;
 //init app
 const app = express();
 
@@ -13,42 +15,47 @@ mongoose
   .then(() => console.log("database connection successful"))
   .catch((error) => console.log(error.message));
 
-  //view engin
+const todoSchema = mongoose.Schema(
+  { title: { type: String, required: true }, desc: String },
+  { timeStamp: true }
+);
+
+//view engin
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname,"public")))
-app.get("/", (req,res,next)=>{
-    try {
-        res.render("index")
-    } catch (error) {
-        res.status(500).json({message:error.message})
-    }
-})
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res, next) => {
+  try {
+    res.render("index", { title: "list todo" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-app.get("/add-todo", (req,res,next)=>{
-    try {
-        res.render("newTodo")
-    } catch (error) {
-        res.status(500).json({message:error.message})
-    }
-})
+app.get("/add-todo", (req, res, next) => {
+  try {
+    res.render("newTodo", { title: "add todo" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-app.get("/update-todo", (req,res,next)=>{
-    try {
-        res.render("updateTodo")
-    } catch (error) {
-        res.status(500).json({message:error.message})
-    }
-})
+app.get("/update-todo", (req, res, next) => {
+  try {
+    res.render("updateTodo", { title: "update todo" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-app.get("/delete-todo", (req,res,next)=>{
-    try {
-        res.render("deleteTodo")
-    } catch (error) {
-        res.status(500).json({message:error.message})
-    }
-})
+app.get("/delete-todo", (req, res, next) => {
+  try {
+    res.render("deleteTodo", { title: "delete todo" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // listen sever
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
