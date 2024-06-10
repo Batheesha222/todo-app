c03bf8eb0b0bf028dade7a6f75f64e68738cc86a
Batheesha222 <batheesha.dilmeen@gmail.com>
2024-06-11 01:22:01 +0530

    Add todo and List todo function complete

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { timeStamp } = require("console");
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-const { title } = require("process");
+const bodyParser = require("body-parser");
+const moment = require("moment")
 
 const PORT = 8000;
 //init app
@@ -16,16 +16,26 @@ mongoose
   .catch((error) => console.log(error.message));
 
 const todoSchema = mongoose.Schema(
-  { title: { type: String, required: true }, desc: String },
-  { timeStamp: true }
+  {
+    title: { type: String, required: true },
+    desc: String,
+  },
+  { timestamps: true }
 );
+
+const Todo = mongoose.model("todo", todoSchema);
 
 //view engin
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.get("/", (req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: true })); //urlencoded means form data...
+
+app.get("/", async (req, res, next) => {
   try {
-    res.render("index", { title: "list todo" });
+    const todos = await Todo.find({}).sort({createdAt:-1});
+
+    res.locals.moment = moment;
+    res.render("index", { title: "list todo" ,todos});
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -50,6 +60,20 @@ app.get("/update-todo", (req, res, next) => {
 app.get("/delete-todo", (req, res, next) => {
   try {
     res.render("deleteTodo", { title: "delete todo" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/add-todo", async (req, res, next) => {
+  try {
+    const { title, desc } = req.body;
+    if (!title) {
+      return res.status(400).json({ message: "title require" });
+    }
+    const newTodo = new Todo({ title, desc });
+    await newTodo.save();
+    res.redirect("/");
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
